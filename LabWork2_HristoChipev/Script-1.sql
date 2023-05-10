350ad721d05664f784185125b24cc3a97cb14e89
Hristo Chipev <107315850+HChipev@users.noreply.github.com>
2023-05-10 17:19:29 +0300

    Lab Work 2 finished exercises

---
+++ LabWork2_HristoChipev/Script-1.sql
@@ -0,0 +1,9 @@
+select * from country where Continent = 'Asia';
+select * from country where (Continent = 'Asia' or Continent = 'Africa') and Population > 100000000;
+select * from country where name in ('Andorra', 'Barbados', 'Yemen', 'Vietnam', 'Poland');
+select * from country where Continent not in ('South America','North America') and GovernmentForm like('%republic%');
+
+select * from country where LifeExpectancy between 60 and 70;
+select * from country where LifeExpectancy > 70;
+select distinct Region from country;
+select * from country order by SurfaceArea asc limit 1; 
