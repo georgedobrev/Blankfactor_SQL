aad4279412cf8d80bc5c4a4b96c5bcf09e302060
Milcho Kasmetov <milcho.kasmetov@blankfactor.com>
2023-05-11 19:09:48 +0300

    LabWork4_MilchoKasmetov

---
+++ LabWork4_MilchoKasmetov/LabWork4_MilchoKasmetov.sql
@@ -0,0 +1,26 @@
+use world;
+
+select count(*) as TotalCountries_With_Independence_After_1970 from country c 
+where c.IndepYear >= 1970;
+
+
+select count(c.Continent) as TotalContinents from country c 
+
+
+select SUM(c.SurfaceArea) as TotalSurfaceArea from country c 
+where c.Continent in ('Europe')
+
+
+select c.Name ,c.SurfaceArea  from country c 
+order by  c.SurfaceArea 
+desc LIMIT 3;
+
+select round(avg(c.Population)) as AvgPopulation,c.CountryCode  from city c  
+group by c.CountryCode  
+order by AvgPopulation;
+
+select c.District ,sum(c.Population) as DistrictPopulation  from city c 
+where c.CountryCode  in ('NLD')
+group by c.District 
+having  DistrictPopulation > 1000000;
+
