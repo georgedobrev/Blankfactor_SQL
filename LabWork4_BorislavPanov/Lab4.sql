222d929c4aae8fbcbe59d4358d0aac2f6cd5d454
Borislav Panov <borislav.panov@blankfactor.com>
2023-05-11 21:22:59 +0300

    Create Lab4.sql

---
+++ LabWork4_BorislavPanov/Lab4.sql
@@ -0,0 +1,25 @@
+-- Task 1
+
+SELECT count(*) AS independent_after_1970 FROM country WHERE IndepYear >= 1970;
+
+-- Task 2
+
+SELECT count(DISTINCT Continent) AS number_of_continents FROM country c;
+
+-- Task 3
+
+SELECT sum(SurfaceArea) AS total_surface_area_europe FROM country c WHERE Continent = 'Europe';
+
+-- Task 4
+
+SELECT continent, sum(SurfaceArea) AS surface_area FROM country c GROUP BY Continent ORDER BY sum(SurfaceArea) DESC LIMIT 3;
+
+-- Task 5
+
+SELECT CountryCode, avg(Population) AS avg_city_population FROM city c2 GROUP BY CountryCode ORDER BY avg(Population);
+
+-- Task 6
+
+SELECT District, sum(Population) FROM CITY WHERE CountryCode = 'NLD' GROUP BY District HAVING sum(Population) > 1000000 ORDER BY sum(Population) DESC;
+
+
