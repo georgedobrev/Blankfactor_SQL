906101e227f97bb1f6681a5184ca2d54f23c5587
borislavpanov-blankfactor <borislav.panov@blankfactor.com>
2023-05-12 11:47:57 +0300

    Create Lab8.sql

---
+++ LabWork8 - BorislavPanov/Lab8.sql
@@ -0,0 +1,32 @@
+-- Task 1
+
+WITH avg_salary_all AS (
+    SELECT AVG(salary) AS avg_salary_all
+    FROM employees
+), 
+avg_salary_hr AS (
+    SELECT AVG(salary) AS avg_salary_hr
+    FROM employees
+    WHERE department_id = 789
+),
+employee_salary AS (
+    SELECT e.employee_id, e.first_name, e.last_name, e.salary
+    FROM employees e
+)
+SELECT es.employee_id, es.first_name, es.last_name, d.department_name,  es.salary,
+       asa.avg_salary_all, ash.avg_salary_hr
+FROM employee_salary es, departments d 
+CROSS JOIN avg_salary_all asa
+CROSS JOIN avg_salary_hr ash;
+
+-- Task 2
+
+SELECT d.department_name, d.Location, m.First_name, m.Last_name FROM departments d
+JOIN managers m ON d.Manager_id = m.Manager_id;
+
+CREATE VIEW view_department AS 
+SELECT d.department_name, d.Location, m.First_name, m.Last_name FROM departments d
+JOIN managers m ON d.Manager_id = m.Manager_id;
+
+
+SELECT * FROM view_department;
