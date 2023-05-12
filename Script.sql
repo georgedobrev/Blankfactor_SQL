e54c5124eb7eead66dfe951f02cbb42d5f745e6a
hristiyanaSh <hristiyana.shopova@blankfactor.com>
2023-05-12 11:46:35 +0300

    homework LabWork_5

---
+++ Script.sql
@@ -0,0 +1,32 @@
+select ifnull(IndepYear, 'N/A') as IndepYear from country ;
+
+select coalesce (indepyear, 'N/A') as IndepYear from country;
+
+select gnPold, GNP, coalesce (GNPold, GNP, 0) as GNPnew from country;
+
+
+
+#TASK1
+
+select ifnull(LifeExpectancy, 'N/A') as LifeExpectancy from country ; 
+
+
+#TASK2
+
+select name, surfacearea 
+(
+if(SurfaceArea > 1000000, 'Large country'),
+if(SurfaceArea between 50000 and 1000000, 'Medium country'),
+if(SurfaceArea between 20000 and 50000, 'Small country'),
+if (SurfaceArea < 20,'Extra small country')
+)
+as CountrySize from country ;
+
+
+select name, surfaceArea,
+case  
+when surfacearea > 1000000 then 'Large country'
+when SurfaceArea between 50000 and 1000000 then 'Medium country'
+when surfaceArea between 20000 and 50000 then 'Small country'
+else 'Extra small country'
+end as CountrySize from country ;
