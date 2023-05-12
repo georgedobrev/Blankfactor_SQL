7cbafe72faa906047c5ba681e798c23745d975b0
Dame Stoilovski <dame.stoilovski@blankfactor.com>
2023-05-12 11:32:57 +0300

    Initiate commite

---
+++ LabWork5_Dame_Stoilovski/LabWork5_Dame_Stoilovski.sql
@@ -0,0 +1,24 @@
+#TASK 1
+select Name
+	, ifnull(LifeExpectancy , 'N/A') as LifeExpectancy 
+from country;
+
+
+#TASK 2
+select SurfaceArea 
+	, case 
+		when SurfaceArea  > 1000000 then 'Large country'
+			when SurfaceArea  > 50000 then 'Medium country'
+				when SurfaceArea  > 20000 then 'Small country'
+					when SurfaceArea  < 20 then 'Extra small country'
+						else 'N/A' end as CountrySize
+from country;
+
+
+select 
+	SurfaceArea 
+		, if(SurfaceArea  > 1000000, 'Large country'
+			, if(SurfaceArea  > 50000, 'Medium country'
+				, if(SurfaceArea  > 20000, 'Small country'
+					, if(SurfaceArea  < 20, 'Extra small country', 'N/A')))) as Age_group
+from country;
