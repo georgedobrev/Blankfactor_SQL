c633025840397112235758881abe7b38445aa477
AntonioNik <antonio.nikolov@blankfactor.com>
2023-05-12 14:14:58 +0300

    LabWork4_Antonio

---
+++ LabWork4_Antonio.sql
@@ -0,0 +1,26 @@
+select count(*) as indep_countries 
+from country 
+where indepYear >= 1970;
+
+select count(*) as countries_count 
+from country;
+
+select sum(surfaceArea) as total_area 
+from country 
+where continent = 'Europe';
+
+select name, surfaceArea 
+from country 
+order by surfaceArea desc 
+limit 3; 
+
+select countryCode, round(avg(Population)) as avg_city_pop
+from city 
+group by countryCode 
+order by avg_city_pop asc;
+
+select district, sum(population) as district_population 
+from city 
+where countryCode = 'NLD' 
+group by district 
+having district_population > 1000000;
