abb00743e2b2bd4304c99cff534392a887fcbcb0
VladislavMilchov <131447185+VladislavMilchov@users.noreply.github.com>
2023-05-15 22:17:22 +0300

    LabWork5_VladislavMilchov

---
+++ LabWork5_VladislavMilchov/Lab5.sql
@@ -0,0 +1,23 @@
+select 
+LifeExpectancy,
+ifnull(LifeExpectancy, 'N/A') as LifeExpectancyNew
+from country;
+
+select 
+SurfaceArea
+ , if(SurfaceArea > 1000000, 'Large countries'
+ , if(SurfaceArea > 50000, 'Medium'
+ , if(SurfaceArea > 20000, 'Small'
+ , 'Extra small'  ))) as SizeOfCpuntry
+from country;
+
+select
+SurfaceArea, 
+case 
+  when SurfaceArea > 1000000 then 'Large countries'
+   when SurfaceArea > 50000 then 'Medium'
+    when SurfaceArea > 20000 then 'Small'
+     else  'Extra small'
+  end as SizeOfcountry
+  from country;
+
