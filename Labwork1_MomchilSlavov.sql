699b3ff3a8398e49c54a122bdac02e6e9a686484
momchilslavov1 <131446908+momchilslavov1@users.noreply.github.com>
2023-05-16 11:42:40 +0300

    Add files via upload

---
+++ Labwork1_MomchilSlavov.sql
@@ -0,0 +1,26 @@
+alter table employees
+add salary decimal(10,2) not null,
+add previous_experience enum('Yes', 'No') not null;
+
+
+alter table departments
+add manager_id int not null,
+add location varchar(255) not null;
+
+
+create table managers
+
+(
+
+	manager_id int primary key,
+	first_name varchar(255) not null,
+	last_name varchar(255) not null,
+	email varchar(255) unique not null,
+	hire_date date not null
+	
+);
+
+alter table departments
+
+add foreign key (manager_id) 
+references managers(manager_id);
