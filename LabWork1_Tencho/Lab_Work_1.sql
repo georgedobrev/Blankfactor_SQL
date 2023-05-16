0dfae777b6db3413d149fd423d7804874c8fab1e
MagicalTencho <tencho.kuyumdzhiev@blankfactor.com>
2023-05-16 14:15:38 +0300

    LabWork1_Tencho

---
+++ LabWork1_Tencho/Lab_Work_1.sql
@@ -0,0 +1,43 @@
+create database if not exists employees_test_Tencho;
+
+use employees_test_Tencho;
+
+
+create table if not exists departments 
+(department_id int primary key,
+department_name varchar(255) not null
+);
+
+create table if not exists employees
+(employee_id int primary key,
+first_name varchar(255) not null,
+last_name varchar(255) not null,
+department_id int,
+email varchar(255) unique,
+hire_date date default '2023-01-01',
+foreign key (department_id) references departments (department_id)
+);
+
+alter table employees 
+add salary decimal(7,2),
+add previous_experience ENUM("yes", "NO");	
+
+
+alter table departments
+add manager_id int,
+add location varchar(255),
+add foreign key (manager_id) references managers (manager_id);
+
+
+
+create table if not exists managers
+(manager_id int primary key,
+first_name varchar(255) not null,
+last_name varchar(255) not null,
+email varchar(255) unique,
+hire_date date default '2023-01-01' not null
+);
+
+
+drop table employees ;
+drop table departments ;
