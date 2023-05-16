dc1117d8c1c119b1474f43f22ddef302d6b05fa1
hristiyanaSh <131446919+hristiyanaSh@users.noreply.github.com>
2023-05-17 00:54:14 +0300

    Update Script-1.sql

--- LabWork2_Hristiyana_Shopova/Script-1.sql
+++ LabWork2_Hristiyana_Shopova/Script-1.sql
@@ -6,7 +6,7 @@ select * from country where (Continent = 'Asia' or Continent = 'Africa') and Pop
 
 select * from country where Name in ( 'Andorra','Barbados', 'Yemen', 'Vietnam', 'Poland');
 
-select * from country where name not like '%America' and GovernmentForm like ('Republic');
+select * from country where name not like '%America' and GovernmentForm like ('%Republic%');
 
 
 #TASK2
