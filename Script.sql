1187b9270f7aead0979c420aec4b82a61f05fc6e
hristiyanaSh <131446919+hristiyanaSh@users.noreply.github.com>
2023-05-17 00:30:42 +0300

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -2,7 +2,7 @@
 #TASK1
 
 insert into employees (employee_id, first_name , last_name ,department_id, Salary, Previous_experience)
-values(4,'Robert', 'Bosch', 789, 245.60, 'no') ;
+values(6,'Robert', 'Bosch', 789, 245.60, 'no') ;
 
 insert into departments (department_id, department_name,Manager_id, Location)
 values(789,'HR', 777, 'Berlin');
