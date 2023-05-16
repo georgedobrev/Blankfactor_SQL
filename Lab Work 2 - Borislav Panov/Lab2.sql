7ea1f9cf2d1229af407e941d784545262ded2b69
Borislav Panov <borislav.panov@blankfactor.com>
2023-05-16 22:13:01 +0300

    Update Lab2.sql

--- Lab Work 2 - Borislav Panov/Lab2.sql
+++ Lab Work 2 - Borislav Panov/Lab2.sql
@@ -2,7 +2,7 @@
 SELECT * FROM country WHERE Continent = 'Asia'; 
 SELECT * FROM country WHERE (Continent = 'Asia' OR Continent = 'Africa') AND Population > 100000000;
 SELECT * FROM country WHERE Name = 'Andorra' OR Name = 'Barbados' OR Name = 'Yemen' OR Name = 'Poland';
-SELECT * FROM country WHERE (Continent <> 'South America' AND Continent <> 'North America') AND GovernmentForm = 'Republic';
+SELECT * FROM country WHERE (Continent <> 'South America' AND Continent <> 'North America') AND GovernmentForm LIKE '%Republic%';
 
 
 -- Task 2 
