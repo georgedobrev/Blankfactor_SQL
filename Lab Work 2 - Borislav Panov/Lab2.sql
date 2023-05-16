502179f6866978bb2faa4d613a976e54f9eaba80
Borislav Panov <borislav.panov@blankfactor.com>
2023-05-16 19:00:33 +0300

    Update Lab2.sql

--- Lab Work 2 - Borislav Panov/Lab2.sql
+++ Lab Work 2 - Borislav Panov/Lab2.sql
@@ -1,10 +1,15 @@
 -- Task 1 
-
-SELECT * FROM country WHERE Continent = "Asia"; 
+SELECT * FROM country WHERE Continent = 'Asia'; 
 SELECT * FROM country WHERE (Continent = 'Asia' OR Continent = 'Africa') AND Population > 100000000;
 SELECT * FROM country WHERE Name = 'Andorra' OR Name = 'Barbados' OR Name = 'Yemen' OR Name = 'Poland';
- -- Task 2 
+SELECT * FROM country WHERE (Continent <> 'South America' AND Continent <> 'North America') AND GovernmentForm = 'Republic';
+
+
+-- Task 2 
  
- SELECT * FROM country WHERE LifeExpectancy BETWEEN 60 AND 70;
- SELECT * FROM country WHERE LifeExpectancy > 70;
- SELECT name, Region FROM country SELECT * FROM country ORDER BY SurfaceArea ASC LIMIT 1; 
+SELECT * FROM country WHERE LifeExpectancy BETWEEN 60 AND 70;
+SELECT * FROM country WHERE LifeExpectancy > 70;
+SELECT DISTINCT Region, COUNT(*) AS NumRegions
+FROM country
+GROUP BY Region;
+SELECT * FROM country ORDER BY SurfaceArea ASC LIMIT 1; 
