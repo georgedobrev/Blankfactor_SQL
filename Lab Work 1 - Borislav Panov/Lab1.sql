fb8c456d5c99959dabd44460cbb97d53eccae2c5
borislavpanov-blankfactor <131446895+borislavpanov-blankfactor@users.noreply.github.com>
2023-05-16 21:59:25 +0300

    Lab Work 1 - Borislav Panov (#28)
    * added tasks
    
    * Update Lab1.sql

---
+++ Lab Work 1 - Borislav Panov/Lab1.sql
@@ -0,0 +1,19 @@
+alter table employees  
+add Salary decimal(5,2),
+add Previous_experience ENUM('Yes', 'No');
+
+
+alter table departments 
+add Manager_id int,
+add Location varchar(255);
+
+create table if not exists managers 
+(
+Manager_id int PRIMARY KEY,
+First_name varchar(255) not null,
+Last_name varchar(255) not null,
+Email varchar(255) unique not null,
+Hire_date date not null
+);
+
+alter table departments add foreign key (Manager_id) references managers(Manager_id);
