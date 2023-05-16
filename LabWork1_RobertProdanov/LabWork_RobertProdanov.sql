46bd4d277b74d56c31496c5e6ceb9316de61ead8
RobertProdanov03 <131447112+RobertProdanov03@users.noreply.github.com>
2023-05-16 22:16:43 +0300

    Update LabWork_RobertProdanov.sql

--- LabWork1_RobertProdanov/LabWork_RobertProdanov.sql
+++ LabWork1_RobertProdanov/LabWork_RobertProdanov.sql
@@ -2,13 +2,13 @@
 
 use employees_test_robertprodanov
 
-alter table employees add salary decimal(10,2) not null;
-alter table employees add previous_experience enum('yes','no') not null;
+alter table employees add salary decimal(10,2);
+alter table employees add previous_experience enum('yes','no');
 
 /*Task2*/
 
-alter table departments add manager_id int not null;
-alter table departments add location varchar(255) not null;
+alter table departments add manager_id int ;
+alter table departments add location varchar(255) ;
 
 /*Task3*/
 
