cded241cc7ebe08b2fb9d1bfbd57ba5ad64add45
LuchoStoymirov <lazar-luchezar.stoymirov@blankfactor.com>
2023-05-16 16:29:22 +0300

    Updated task

--- Tasks.sql
+++ Tasks.sql
@@ -5,11 +5,14 @@ where IndepYear >= 1970;
 
 select count(distinct continent) as continents from country c;
 
+select sum(SurfaceArea) as total_sur_area from country c 
+where Continent = 'Europe';
+
 select sum(SurfaceArea) as combined_surface_area, Continent  from country c 
 group by Continent order by combined_surface_area desc limit 3;
 
-select Code, avg(population) as avarage_population from country c
-group by Code order by avarage_population asc;
+select CountryCode, avg(population) as avarage_population from city c
+group by CountryCode order by avarage_population asc;
 
 select District, sum(population) as district_population from city c
 where CountryCode = 'NLD' group by District having district_population >= 1000000;
