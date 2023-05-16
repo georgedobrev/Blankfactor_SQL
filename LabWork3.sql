211c2b07f10716e73f692399203404166200774c
aleksandararsov <aleksandar.arsov@blankfactor.com>
2023-05-16 12:21:44 +0300

    LabWork3_Aleksandar_Arsov

---
+++ LabWork3.sql
@@ -0,0 +1,21 @@
+use employees_aleksandararsov;
+
+insert into employees (
+employee_id,
+first_name ,
+last_name ,
+email ,
+hire_date ,
+salary ,
+previous_experience ,
+department_id 
+)
+values (6,'Aleksandar','Arsov','email@email.com','2023-01-01',600,'yes', 789);
+
+commit;
+
+update employees 
+set salary = 120
+where employee_id  = 4;
+
+commit;
