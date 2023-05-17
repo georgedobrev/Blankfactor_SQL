400d1cb04749f0460e95bc69693d82a23a95338d
valentinArnautski <valentin.arnautski@blankfactor.com>
2023-05-17 09:12:47 +0300

    Replaced double quotes with single.

--- LabWork2_ValentinArnautski/LabWork2_ValentinArnautski.sql
+++ LabWork2_ValentinArnautski/LabWork2_ValentinArnautski.sql
@@ -1,8 +1,8 @@
 use world;
 
-select * from country where  Continent = "Asia";
+select * from country where  Continent = 'Asia';
 select * from country where (Continent = 'Asia' or Continent = 'Africa') and Population > 100000000;
-select * from country where name in ("Andorra", "Barbados", "Yemen", "Vietnam", "Poland");
+select * from country where name in ('Andorra', 'Barbados', 'Yemen', 'Vietnam', 'Poland');
 select * from country where Continent not in ('South America', 'North America') and GovernmentForm like ('%republic%');
 
 select * from country where LifeExpectancy between 60 and 70;
