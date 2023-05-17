d2c510c3e6fe0c9c61d9ca5e3d73fd595fc50d6c
Kasmetov <milcho.kasmetov@blankfactor.com>
2023-05-17 11:27:27 +0300

    Requested changes done

--- LabWork4_MilchoKasmetov/LabWork4_MilchoKasmetov.sql
+++ LabWork4_MilchoKasmetov/LabWork4_MilchoKasmetov.sql
@@ -7,14 +7,17 @@ where c.IndepYear >= 1970;
 select count(DISTINCT c.Continent) as TotalContinents from country c 
 
 
-select round( SUM(c.SurfaceArea)) as TotalSurfaceArea from country c 
-group by c.Continent in ('Europe')
+select  SUM(c.SurfaceArea) as TotalSurfaceArea from country c 
+where  c.Continent in ('Europe')
 
 
 
-select c.Name ,c.SurfaceArea  from country c 
-order by  c.SurfaceArea 
-desc LIMIT 3;
+select Continent, round(sum(SurfaceArea)) as continentSize
+from country
+group by Continent 
+order by continentSize desc
+limit 3;
+
 
 select round(avg(c.Population)) as AvgPopulation,c.CountryCode  from city c  
 group by c.CountryCode  
