e4b8a05431bb3cb0bdb6b2f4ac5d1cc973134175
valentinArnautski <131446923+valentinArnautski@users.noreply.github.com>
2023-05-17 09:20:18 +0300

    Update LabWork5_ValentinArnautski.sql

--- LabWork5_ValentinArnautski/LabWork5_ValentinArnautski.sql
+++ LabWork5_ValentinArnautski/LabWork5_ValentinArnautski.sql
@@ -1,23 +1,22 @@
-select name, ifnull(LifeExpectancy , 'N/A') as NoLifeExpextancy from country; 
+select name, IFNULL(LifeExpectancy, 'N/A') as NoLifeExpectancy from country;
 
 
 select name, SurfaceArea,
-case
-	when surfaceArea > 10000000 then 'Large'
-			when surfaceArea > 50000 then 'Medium'
-					when surfaceArea > 20000 then 'Small'
-							else 'Extra Small'
-end as 'Countries Size'
-from country
-
-select name, SurfaceArea,
-
-	if(SurfaceArea  > 10000000, 'Large',
-		if(SurfaceArea > 50000, 'Medium',
-			if(SurfaceArea  > 20000, 'Small',
-				'Extra Small'))) as 'Countries Size'
+    case
+        when SurfaceArea > 10000000 then 'Large'
+        when SurfaceArea > 50000 then 'Medium'
+        when SurfaceArea > 20000 then 'Small'
+        ELSE 'Extra Small'
+    end as 'Countries Size'
 from country;
 
+
+select name, SurfaceArea,
+    if(SurfaceArea > 10000000, 'Large',
+        if(SurfaceArea > 50000, 'Medium',
+            if(SurfaceArea > 20000, 'Small',
+                'Extra Small'))) as 'Countries Size'
+from country;
 
 
 
