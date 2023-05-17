3a4687fac17868c8e5bdc05062b8200a9f0b007e
RobertProdanov03 <131447112+RobertProdanov03@users.noreply.github.com>
2023-05-17 09:12:39 +0300

    Update LabWork6_RobertProdanov.sql

--- LabWork6/LabWork6_RobertProdanov.sql
+++ LabWork6/LabWork6_RobertProdanov.sql
@@ -4,6 +4,7 @@ select c.Name, c.CountryCode, co.Name, co.Continent
 from city as c
 inner join country as co
 on co.Code = c.CountryCode;
+order by c.name asc; 
 
 /*Task2*/
 
@@ -15,4 +16,19 @@ where country.Continent = 'Europe';
 
 /*Task3*/
 
+select
+round(avg(c.population)) as avg_city_population, cn.name as countryName
+from city c 
+inner join country cn on c.countryCode = cn.code
+group by cn.name order by avg_city_population asc; 
+
+/*Task4*/
+
+select
+c.name, count(c2.IsOfficial) as numOfficialLang
+from country c
+left join countrylanguage c2 on c.code = c2.countryCode and c2.IsOfficial = 'T'
+group by c.name 
+having numOfficialLang >= 3
+order by numOfficialLang desc;
 
