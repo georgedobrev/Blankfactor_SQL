1aa7697353c8bedae135aa42bfdb985374bc01e6
kolevgenchevwork <131447168+kolevgenchevwork@users.noreply.github.com>
2023-05-18 16:05:02 +0300

    FixedMistakes

--- LabWork2_BozhidarGenchev.sql
+++ LabWork2_BozhidarGenchev.sql
@@ -1,14 +1,20 @@
 -- The first task
 
-select * from country where Continent = 'Asia'; 
-select * from country where (Continent = 'Asia' or Continent = 'Africa') and Population > 100000000;
-SELECT * from country where Name = 'Andorra' OR Name = 'Barbados' OR Name = 'Yemen' OR Name = 'Poland';
-select * from country where (Continent <> 'South America' and Continent <> 'North America') and GovernmentForm like '%Republic%';
 
---The second task
-select * from country where LifeExpectancy between 60 and 70;
-select * from country where LifeExpectancy > 70;
-select distinct Region, COUNT(*) as NumRegions
-from country
-group by Region;
-select * from country order by SurfaceArea asc limit 1; 
+SELECT * FROM country WHERE Continent IN ('Asia');
+
+
+SELECT * FROM country WHERE Continent IN ('Asia', 'Africa') AND Population > 100000000;
+
+
+SELECT * FROM country WHERE Name IN ('Andorra', 'Barbados', 'Yemen', 'Poland');
+
+
+SELECT * FROM country WHERE Continent NOT IN ('South America', 'North America') AND GovernmentForm LIKE '%Republic%';
+
+
+SELECT * FROM country WHERE LifeExpectancy BETWEEN 60 AND 70;
+SELECT * FROM country WHERE LifeExpectancy > 70;
+SELECT Region, COUNT(*) AS NumRegions FROM country GROUP BY Region;
+SELECT * FROM country ORDER BY SurfaceArea ASC LIMIT 1; 
+
