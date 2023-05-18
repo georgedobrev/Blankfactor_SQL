0ba048b512883a8437558deba3652cf645469050
aleksandramihaylova <131447061+aleksandramihaylova@users.noreply.github.com>
2023-05-18 14:08:43 +0300

    LabWork7_AleksandraMihaylova

---
+++ LabWork7_AleksandraMihaylova/Script_LabWork7_AleksandraMihaylova.sql
@@ -0,0 +1,28 @@
+select employee_id, first_name, last_name, department_id, email, hire_date, salary, previous_experience  
+from employees
+union
+select employee_id, first_name, last_name, 
+null as department_id, null as email, null as hire_date, null as salary, null as previous_experience
+from employees_duplicate
+;
+
+select c.Name, c.District, c.CountryCode
+from city as c
+where c.CountryCode in (
+select c2.Code from country as c2
+where c2.Continent = 'Europe'
+);
+
+select c2.Name, sum(c.Population) as population_cities
+from country as c2
+join city as c
+on c2.Code = c.CountryCode
+where c2.Code in (
+select c.CountryCode
+from city as c
+group by c.CountryCode
+having sum(c.Population) > 1000000
+)
+group by c2.code, c2.Name
+order by cities_population desc
+;
