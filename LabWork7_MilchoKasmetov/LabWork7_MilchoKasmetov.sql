b1a4cb9ad11d1ccdd994b6ef3915f0f91ef25606
Milcho Kasmetov <milcho.kasmetov@blankfactor.com>
2023-05-19 15:02:22 +0300

    Update LabWork7_MilchoKasmetov.sql

--- LabWork7_MilchoKasmetov/LabWork7_MilchoKasmetov.sql
+++ LabWork7_MilchoKasmetov/LabWork7_MilchoKasmetov.sql
@@ -1,17 +1,16 @@
 USE employees_milcho;
 
-select *
+select employee_id, first_name, last_name, email, hire_date, department_id, salary
 from employees
 union
 select
 employee_id
 , first_name
 , last_name
-,null 
-,null 
-,null 
-,null 
-,null 
+,null as email
+,null as hire_date
+,null as department_id
+,null as salary
 from employees_duplicate;
 
 
@@ -21,7 +20,7 @@ select c.Name ,c.District ,c.CountryCode  from city c
 where CountryCode in (select c2.Code  from country c2 where c2.Continent in ('Europe')) 
 
 
-select * from country c 
+select c.Name ,c.Population  from country c 
 where c.Code in (select c2.CountryCode  from city c2
 group by c2.CountryCode
 having sum(c2.Population) > 1000000);
