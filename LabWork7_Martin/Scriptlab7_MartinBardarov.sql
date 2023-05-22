78a94addd521de2181dc2e3476142b983f0d6783
martinbardarov-exe <martin.bardarov@blankfactor.com>
2023-05-22 09:07:27 +0300

    update

---
+++ LabWork7_Martin/Scriptlab7_MartinBardarov.sql
@@ -0,0 +1,47 @@
+use employees_martin
+use world;
+
+select
+	employee_id,
+	first_name,
+	last_name,
+	department_id,
+	email,
+	hire_date,
+	salary,
+	previous_experience
+from
+	employees
+union
+
+select
+	employee_id,
+	first_name,
+	last_name,
+	null as department_id,
+	null as email,
+	null as hire_date,
+	null as salary,
+	null as previous_experience
+from
+	employees_duplicate;
+
+select c.Name, c.District, c.CountryCode
+from city as c
+where c.CountryCode in (
+select co.Code from country as co
+where co.Continent = 'Europe'
+);
+
+select co.Code, co.Name, sum(c.Population) as cities_population
+from country as co
+join city as c
+on co.Code = c.CountryCode
+where co.Code in (
+	select c.CountryCode
+	from city as c
+	group by c.CountryCode
+	having sum(c.Population) > 1000000
+)
+group by co.code, co.Name
+order by cities_population desc;
