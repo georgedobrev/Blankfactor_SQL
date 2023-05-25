c2fea83a64c61a45a454b100cb2e2efe46205a4e
Dame Stoilovski <131446912+DameStoilovski99@users.noreply.github.com>
2023-05-25 22:13:29 +0300

    LabWork3_Dame_Stoilovski (#14)
    * Initial commit
    
    * Second commit
    
    * Update Lab_Work_3_Dame_Stoilovski.sql
    
    * Delete .DS_Store

---
+++ LabWork_3_Dame_Stoilovski/Lab_Work_3_Dame_Stoilovski.sql
@@ -0,0 +1,26 @@
+use employees_dame_Stoilovski
+
+insert into employees (
+employee_id,
+first_name,
+last_name,
+email,
+hire_date,
+Salary,
+Previous_exeprience,
+department_id)
+values
+(6, 'Dame', 'Stoilovski', 'dame@email.com', '2023-02-03', 1000, 'yes', 789);
+
+commit;
+
+select * from employees e 
+
+update employees 
+set salary = 120, department_id = 789
+where employee_id  = 4;
+
+commit;
+
+select * from departments d 
+select * from employees e 
