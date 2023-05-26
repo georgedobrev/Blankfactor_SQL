0f1a0ad5f00d5ed9bade870492c14af1e3c8734f
valentinArnautski <valentin.arnautski@blankfactor.com>
2023-05-26 10:26:13 +0300

    Changes

--- LabWork6_ValentinArnautski/LabWork6_ValentinArnautski.sql
+++ LabWork6_ValentinArnautski/LabWork6_ValentinArnautski.sql
@@ -1,32 +1,25 @@
 select c.Code , c.Name as CountryName, c.Continent, c2.Name as cityName from country c 
 inner join city c2 on c.Code = c2.CountryCode;
 
-select c.Code , c.Name as CountryName, c.Continent, c2.Name as cityName from country c 
-inner join city c2 on c.Code = c2.CountryCode where c.Continent = 'Europe'
+select c.Name as city_name, c.CountryCode, c2.Name as country_name, c2.Continent 
+from city c 
+inner join country c2 on c2.Code = c.CountryCode
+where c2.Continent = 'Europe'
+order by c.Name asc;
 
+select c2.Name, avg(c.Population) as avarage_population
+from city c 
+inner join country c2 
+on c.CountryCode = c2.Code 
+group by c2.Name 
+order by avarage_population asc;
 
-select
-round(avg(c1.population)) as avg_city_population, c2.name as countryName
-from city c1 
-inner join country c2 on c1.countryCode = c2.code
-group by c2.name order by avg_city_population asc; 
-
-select
-c1.name, sum(c2.IsOfficial) as officialLanguages
-from country c1
-left join countrylanguage c2 on c1.code = c2.countryCode
-where c2.IsOfficial = "T"
-group by c1.name 
-having num_official_lang >= 3
-order by num_official_lang desc;
-
-
-
-select * from city c1;
-select * from country c2;
-select * from countrylanguage l;
-
-
+select c.Name, count(c2.`Language`) as official_language_count
+from country c 
+inner join countrylanguage c2 on c2.CountryCode = c.Code 
+group by c2.IsOfficial, c.Name 
+having c2.IsOfficial = 'T' and official_language_count >= 3
+order by official_language_count desc;
 
 
 
