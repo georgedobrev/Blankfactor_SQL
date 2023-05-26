16339467d6eea65ab5c43693c019cf50c40267ea
valentinArnautski <valentin.arnautski@blankfactor.com>
2023-05-26 08:54:18 +0300

    Changes

--- LabWork5_ValentinArnautski/LabWork5_ValentinArnautski.sql
+++ LabWork5_ValentinArnautski/LabWork5_ValentinArnautski.sql
@@ -1,22 +1,23 @@
-select name, IFNULL(LifeExpectancy, 'N/A') as NoLifeExpectancy from country;
+select name, ifnull(LifeExpectancy , 'N/A') as NoLifeExpextancy from country; 
 
 
 select name, SurfaceArea,
-    case
-        when SurfaceArea > 10000000 then 'Large'
-        when SurfaceArea > 50000 then 'Medium'
-        when SurfaceArea > 20000 then 'Small'
-        ELSE 'Extra Small'
-    end as 'Countries Size'
-from country;
-
+case
+	when surfaceArea > 10000000 then 'Large'
+			when surfaceArea > 50000 then 'Medium'
+					when surfaceArea > 20000 then 'Small'
+							else 'Extra Small'
+end as 'Countries Size'
+from country
 
 select name, SurfaceArea,
-    if(SurfaceArea > 10000000, 'Large',
-        if(SurfaceArea > 50000, 'Medium',
-            if(SurfaceArea > 20000, 'Small',
-                'Extra Small'))) as 'Countries Size'
+
+	if(SurfaceArea  > 10000000, 'Large',
+		if(SurfaceArea > 50000, 'Medium',
+			if(SurfaceArea  > 20000, 'Small',
+				'Extra Small'))) as 'Countries Size'
 from country;
+
 
 
 
