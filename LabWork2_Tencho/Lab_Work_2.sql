6df2ead8c569347b7f732464b1e63ce8f6e26a8a
MagicalTencho <tencho.kuyumdzhiev@blankfactor.com>
2023-05-26 11:47:04 +0300

    Requested Changes_LabWork2

--- LabWork2_Tencho/Lab_Work_2.sql
+++ LabWork2_Tencho/Lab_Work_2.sql
@@ -1,10 +1,17 @@
+#LAB WORK 2
 use world;
 
-
+#task 1:
 select * from country where Continent = 'Asia';
 select * from country where Continent in ('Asia', 'Africa') and Population > 100000000;
-select * from country where Name like ('Andorra') or Name like ('Barbados') or Name like ('Yemen') or Name like ('Vietnam') or Name like ('Poland');
-select * from country where Name like ('Andorra' or 'Barbados' or 'Yemen' or 'Vietnam' or 'Poland');
+select * from country where Name in ('Andorra', 'Barbados', 'Yemen', 'Vietnam', 'Poland');
+select * from country where Continent not in ( 'South America', 'North America') and GovernmentForm in ('Republic','Socialistic Republic','Republic');
+
+#task 2:
+select * from country where LifeExpectancy between 60 and 70;
+select * from country where LifeExpectancy > 70;
+select distinct Region from country;
+select * from country order by SurfaceArea limit 1;
 
 
 
