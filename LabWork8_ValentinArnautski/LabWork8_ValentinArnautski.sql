624dc2130d89f4a6de8d132b60cb38deed6aa3f7
valentinArnautski <valentin.arnautski@blankfactor.com>
2023-05-26 10:44:52 +0300

    Changes

--- LabWork8_ValentinArnautski/LabWork8_ValentinArnautski.sql
+++ LabWork8_ValentinArnautski/LabWork8_ValentinArnautski.sql
@@ -1,33 +1,45 @@
-with Avg_Salary_All_CTE as (
-  select  avg(salary) as avg_salary
-  from employees
+with cte1 as (
+select
+avg(salary) as avg_salary
+from employees
 ), 
-Avg_Salary_HR_CTE as (
-  select e.department_id, d.department_name, avg(e.salary) as Avg_Salary_HR 
-  from employees e
-  join departments d on e.department_id = d.department_id
-  where d.department_name = 'HR'
-  group by e.department_id, d.department_name
+cte2 as(
+select
+department_name,
+e.department_id,
+avg(salary) as avg_dep_salary
+from employees e 
+join departments d on e.department_id  = d.department_id 
+group by department_id
+having department_name = "HR"
 )
-select  e.first_name, e.last_name, e.salary, al.avg_salary as Avg_Salary_All, hr.Avg_Salary_HR
-from employees e
-join Avg_Salary_All_CTE al
-join Avg_Salary_HR_CTE hr on e.department_id = hr.department_id
-where e.department_id = hr.department_id
+select 
+first_name, 
+salary, 
+cte1.avg_salary, 
+cte2.department_name, 
+cte2.avg_dep_salary
+from cte1 join cte2 join employees
 
 
-create temporary table temp_ManagersDeparment
-select m.first_name  as manager_fullname, d.department_name, d.location from departments d
-inner join managers m on m.manager_id  = d.manager_id ;
 
-select * from temp_ManagersDeparment
+create temporary table temp_department
+select
+d.department_name, 
+d.location,
+concat(m.first_name , ' ', m.last_name) as manager
+from departments d 
+join managers m on d.manager_id = m.manager_id;
 
+create view view_department as
+select
+d.department_name, 
+d.location,
+concat(m.first_name , ' ', m.last_name) as manager
+from departments d 
+join managers m on d.manager_id = m.manager_id;
 
-create or replace view view_ManagerDepartment as
-select concat(m.first_name,' ' ,m.last_name) as Manager_Full_Name , d.department_name ,d.location from departments d 
-inner join managers m on m.manager_id = d.manager_id ;
-
-select * from view_ManagerDepartment
-
+select * from temp_department;
+select * from view_department;
 
 
