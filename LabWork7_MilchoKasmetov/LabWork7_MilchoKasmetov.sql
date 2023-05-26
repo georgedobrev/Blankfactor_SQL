0f001918b7eda8e19a40f5652b552c3f743b4224
Kasmetov <milcho.kasmetov@blankfactor.com>
2023-05-26 12:03:19 +0300

    Update LabWork7_MilchoKasmetov.sql

--- LabWork7_MilchoKasmetov/LabWork7_MilchoKasmetov.sql
+++ LabWork7_MilchoKasmetov/LabWork7_MilchoKasmetov.sql
@@ -20,7 +20,14 @@ select c.Name ,c.District ,c.CountryCode  from city c
 where CountryCode in (select c2.Code  from country c2 where c2.Continent in ('Europe')) 
 
 
-select c.Name ,sum(c2.population) as population_sum  from country c 
-where c.Code in (select c2.CountryCode  from city c2
-group by c2.CountryCode
-having sum(c2.Population) > 1000000);
+select c1.name, sum(c2.population) as population_sum
+from country c1
+join city c2 on c1.code = c2.countryCode
+where c1.code in (
+    select countryCode
+    from city
+    group by countryCode
+    having sum(population) > 1000000
+)
+group by c1.name
+order by population_sum desc;
