63e0a3659a6ecf7e7603516c2809ce079aec723b
hristiyanaSh <131446919+hristiyanaSh@users.noreply.github.com>
2023-05-27 21:05:47 +0300

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -16,7 +16,7 @@ select Name, ifnull(LifeExpectancy, 'N/A') as LifeExpectancy from country where 
 
 select name, SurfaceArea,
 if(SurfaceArea > 1000000, 'Large country',
-if(SurfaceArea between 50000 and 1000000, 'Medium country',
+if(SurfaceArea between 50001 and 1000000, 'Medium country',
 if(SurfaceArea between 20000 and 50000, 'Small country','Extra small country')))
 as CountrySize from country ;
 
@@ -29,28 +29,4 @@ when surfaceArea between 20000 and 50000 then 'Small country'
 else 'Extra small country'
 end as CountrySize from country ;
 
-
-#TASK1
-
-select count(*) from country  where IndepYear >= 1970;
-
-#TASK2
-
-select count(distinct Continent) from country;
-
-#Task3
-
-select sum(SurfaceArea) from country where Continent = 'Europe';
-
-#TASK4
-
-select Continent, round(sum(SurfaceArea)) as continentSize  from country group by Continent order by continentSize desc limit 3;
-
-#TASK5
-
-select CountryCode , avg(Population) as avg_city_population from city group by CountryCode order by avg(Population) ;
-
-#TASK6
-
-select sum(Population), District from city  where CountryCode = 'NLD' group by District having sum(Population) > 1000000 order by sum(Population) desc;
 
