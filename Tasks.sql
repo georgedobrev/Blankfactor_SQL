d3bbfd5dc4df45909c05bcf784b0a49e08414849
LuchoStoymirov <lazar-luchezar.stoymirov@blankfactor.com>
2023-05-28 11:40:11 +0300

    Rounded where needed

--- Tasks.sql
+++ Tasks.sql
@@ -5,13 +5,13 @@ where IndepYear >= 1970;
 
 select count(distinct continent) as continents from country c;
 
-select sum(SurfaceArea) as total_sur_area from country c 
+select round(sum(SurfaceArea),0) as total_sur_area from country c 
 where Continent = 'Europe';
 
 select sum(SurfaceArea) as combined_surface_area, Continent  from country c 
 group by Continent order by combined_surface_area desc limit 3;
 
-select CountryCode, avg(population) as avarage_population from city c
+select CountryCode, round(avg(population),0) as avarage_population from city c
 group by CountryCode order by avarage_population asc;
 
 select District, sum(population) as district_population from city c
