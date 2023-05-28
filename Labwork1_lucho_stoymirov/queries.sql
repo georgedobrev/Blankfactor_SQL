ee950f8b31256017cf4757f50829441f432c0a28
LuchoStoymirov <lazar-luchezar.stoymirov@blankfactor.com>
2023-05-28 11:36:37 +0300

    Complied with required changes

--- Labwork1_lucho_stoymirov/queries.sql
+++ Labwork1_lucho_stoymirov/queries.sql
@@ -1,17 +1,17 @@
  
 alter table employees_lucho_stoymirov.employees
-add Salary decimal(10,2)
+add Salary decimal(10,2),
 add Previous_expirience enum("Yes", "no");
 
 
 
 alter table employees_lucho_stoymirov.departments
-add Manager_id int
+add Manager_id int,
 add Location varchar(255);
 
 
 create table employees_lucho_stoymirov.managers(
-Manager_id int not null primary key,
+Manager_id int not null,
 First_name varchar(255) not null,
 Last_name  varchar(255) not null,
 Email varchar(320) not null,
