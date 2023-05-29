92e6d638a32829608bd37d27ba48a481d9ac5db2
DenislavHristanov <131447322+DenislavHristanov@users.noreply.github.com>
2023-05-29 15:29:44 +0300

    LabWork1-Denislav Hristanov

---
+++ LabWork1-Denislav_Hristanov/LabWork1.sql
@@ -0,0 +1,17 @@
+use denislav_test;
+
+alter table employees add salary decimal(10,2);
+alter table employees add previous_experience enum('yes','no');
+
+alter table departments add manager_id int ;
+alter table departments add location varchar(255);
+
+create table managers (
+manager_id int primary key,
+first_name varchar(255) not null,
+last_name varchar(255) not null,
+email varchar(255) not null unique,
+hire_date date not null
+);
+
+alter table departments add foreign key (manager_id) references managers(manager_id); 
