75aec81767ac5f47bfe6bfccb83aa4114ba69b07
dimovv23 <georgi.dimov@blankfactor.com>
2023-05-29 12:29:23 +0300

    Applied the requested changes

--- LabWork8_Georgi_Dimov/Lab8.sql
+++ LabWork8_Georgi_Dimov/Lab8.sql
@@ -1,37 +1,38 @@
 --Task1
 
-with cte1 as
-(
-	select department_id, avg(salary) as avg_salary
-	from employees
-	group by department_id
+with cte as (
+    select round(avg(salary)) as avg_salary
+    from employees
 ),
-cte2 as
-(
-	select avg(salary) as avg_hr_department_salary
-	from employees as e2
-	join departments as d2
-	on e2.department_id = d2.department_id
-	where d2.department_name = 'HR'
+cte2 as (
+    select 
+        department_name,
+        e.department_id,
+        round(avg(salary)) as avg_salary_hr
+    from employees e
+    join departments d on e.department_id = d.department_id
+    where department_name = 'HR'
+    group by department_id
 )
-select e.first_name, d.department_name, e.salary, cte1.avg_salary, cte2.avg_hr_department_salary
-from employees as e
-join departments as d
-on e.department_id = d.department_id
-join cte1 on e.department_id = cte1.department_id
-join cte2
-;
+select 
+    e.first_name,
+    e.salary,
+    cte.avg_salary,
+    cte2.avg_salary_hr
+from employees e
+join cte
+join cte2;
 
 --Task2
 
 create temporary table temp_department
-select d.department_name, d.location, m.first_name, m.last_name from departments as d
+select d.department_name, d.location, concat(m.first_name," " ,m.last_name) as manager_name from departments as d
 join managers as m
 on d.manager_id = m.manager_id
 ;
 
 create or replace view view_department as
-select d.department_name, d.location, m.first_name, m.last_name
+select d.department_name, d.location, concat(m.first_name," " ,m.last_name) as manager_name
 from departments as d
 join managers as m
 on d.manager_id = m.manager_id
