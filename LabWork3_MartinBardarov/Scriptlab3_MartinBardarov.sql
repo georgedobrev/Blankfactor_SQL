760c5935790184073299334dd9994f3a790cbf11
martinbardarov-exe <martin.bardarov@blankfactor.com>
2023-05-30 10:03:32 +0300

    update

--- LabWork3_MartinBardarov/Scriptlab3_MartinBardarov.sql
+++
@@ -1,9 +0,0 @@
-
-
-insert into employees
-values
-(6, 'Martin', 'Bardarov', 'bardarov@email.com', '2023-04-19', 789, 800, 'yes');
-
-commit;
-
-update employees set department_id = 789,salary = 120 where employee_id = 4;
