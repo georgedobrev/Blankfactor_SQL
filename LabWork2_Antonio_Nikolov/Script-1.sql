90f019bf453df24fca3bac7d8a7dc2c867e62249
AntonioNik <131446929+AntonioNik@users.noreply.github.com>
2023-05-30 09:43:02 +0300

    LabWork2_Antonio_Nikolov (#36)
    * completed second lab
    
    * Update Script-1.sql
    
    * Update Script-1.sql

---
+++ LabWork2_Antonio_Nikolov/Script-1.sql
@@ -0,0 +1,45 @@
+use world;
+
+select *
+from country
+where Continent = 'Asia';
+
+select *
+from country
+where (Continent = 'Asia' or Continent = 'Africa') and Population > 100000000;
+
+
+select *
+from country
+where Name in ('Andorra', 'Barbados', 'Yemen', 'Vietnam', 'Poland');
+
+
+select *
+from country
+where Continent not in ('South America', 'North America') and GovernmentForm like ('%republic%');
+
+
+//// Task 2////
+
+select *
+from country
+where LifeExpectancy between 60 and 70;
+
+
+select *
+from country
+where LifeExpectancy > 70;
+
+select Region,
+count(*) as num_region
+from country
+group by Region;
+
+select *
+from country 
+order by SurfaceArea 
+asc 
+limit 1;
+
+
+
