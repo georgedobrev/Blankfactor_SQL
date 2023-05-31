7c5e0e3802a6dc9155723d0a91fb617d297a8e47
martinbardarov-exe <martin.bardarov@blankfactor.com>
2023-05-31 11:18:32 +0300

    update

---
+++ LabWork3_MartinBardarov/Script-2LabWork3.sql
@@ -0,0 +1,29 @@
+use employees_martin;
+
+INSERT INTO managers
+(manager_id, first_name, last_name, email, hire_date)
+VALUES(9, 'Marto', 'Bardarov', 'marsto@blankfactor.com', '2023-05-05');
+
+
+INSERT INTO departments 
+(department_id, department_name, manager_id, location)
+VALUES(766, 'HR', 777, 'Sofia');
+
+
+
+INSERT INTO employees
+(employee_id, first_name, last_name, email, hire_date,department_id,  salary, previous_experience)
+VALUES(666, 'Marto', 'Bardarov','marto@blankfactor.com', '2023-05-05' ,766, 2500, 'Yes');
+
+
+
+UPDATE employees
+SET  salary=120
+WHERE employee_id=4;
+
+
+select * from  employees
+select * from  departments
+select * from managers 
+
+ 
