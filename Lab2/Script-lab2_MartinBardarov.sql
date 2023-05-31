63d8eccf0f10b3c9f5d110ac8d4399504cdc71a2
martinbardarov-exe <131447202+martinbardarov-exe@users.noreply.github.com>
2023-05-31 11:01:55 +0300

    Labwork2 martin bardarov (#69)
    * task
    
    * update
    
    * Update Script-lab2_MartinBardarov.sql

---
+++ Lab2/Script-lab2_MartinBardarov.sql
@@ -0,0 +1,14 @@
+
+SELECT * FROM country WHERE Continent = 'Asia'; 
+SELECT * FROM country WHERE (Continent = 'Asia' OR Continent = 'Africa') AND Population > 100000000;
+SELECT * FROM country WHERE Name = 'Andorra' OR Name = 'Barbados' OR Name = 'Yemen' OR Name = 'Poland';
+SELECT * FROM country WHERE (Continent <> 'South America' AND Continent <> 'North America') AND GovernmentForm LIKE '%Republic%';
+
+
+ 
+SELECT * FROM country WHERE LifeExpectancy BETWEEN 60 AND 70;
+SELECT * FROM country WHERE LifeExpectancy > 70;
+SELECT DISTINCT Region, COUNT(*) AS NumRegions
+FROM country
+GROUP BY Region;
+SELECT * FROM country ORDER BY SurfaceArea ASC LIMIT 1; 
